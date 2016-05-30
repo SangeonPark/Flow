d2dd8788e195a64523c19288522b88b0bfae52b4
Sangeon <sp35@rice.edu>
2016-05-30 11:58:35 +0200

    2nd commit

--- V2Analyzer/src/V2Analyzer.cc
+++ V2Analyzer/src/V2Analyzer.cc
@@ -19,40 +19,10 @@
 
 
 // system include files
-#include <memory>
-
-// user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-//
-// class declaration
-//
-
-class V2Analyzer : public edm::EDAnalyzer {
-   public:
-      explicit V2Analyzer(const edm::ParameterSet&);
-      ~V2Analyzer();
-
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+#include "Flow/V2Analyzer/interface/V2Analyzer.h"
 
 
-   private:
-      virtual void beginJob() ;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-      virtual void endJob() ;
 
-      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
-      virtual void endRun(edm::Run const&, edm::EventSetup const&);
-      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-
-      // ----------member data ---------------------------
-};
 
 //
 // constants, enums and typedefs
@@ -68,6 +38,16 @@ class V2Analyzer : public edm::EDAnalyzer {
 V2Analyzer::V2Analyzer(const edm::ParameterSet& iConfig)
 
 {
+    //    nHitCut_ = iConfig.getParameter<int>("nHitCut");
+    dxySigCut_ = iConfig.getParameter<double>("dxySigCut");
+    dzSigCut_ = iConfig.getParameter<double>("dzSigCut");
+    etaCutMin_ = iConfig.getParameter<double>("etaCutMin");
+    etaCutMax_ = iConfig.getParameter<double>("etaCutMax");
+    NTrkMin_ = iConfig.getParameter<int>("NTrkMin");
+    NTrkMax_ = iConfig.getParameter<int>("NTrkMax");
+    
+    trackSrc_ = iConfig.getParameter<edm::InputTag>("trackSrc");
+    vertexSrc_ = iConfig.getParameter<std::string>("vertexSrc");
    //now do what ever initialization is needed
 
 }
@@ -90,32 +70,130 @@ V2Analyzer::~V2Analyzer()
 void
 V2Analyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+    using namespace edm;
+    using namespace std;
+
+        edm::Handle<reco::VertexCollection> vertices;
+    iEvent.getByLabel(vertexSrc_,vertices);
+    double bestvz=-999.9, bestvx=-999.9, bestvy=-999.9;
+    double bestvzError=-999.9, bestvxError=-999.9, bestvyError=-999.9;
+    const reco::Vertex & vtx = (*vertices)[0];
+    bestvz = vtx.z(); bestvx = vtx.x(); bestvy = vtx.y();
+    bestvzError = vtx.zError(); bestvxError = vtx.xError(); bestvyError = vtx.yError();
+
+    //vertices selection
+    if(bestvz < -15.0 || bestvz>15.0) return;
+
+//    cout << "test: " << bestvz << bestvx << bestvy << bestvzError << bestvxError << bestvyError;
+    
+    
+    edm::Handle<reco::TrackCollection> tracks;
+    iEvent.getByLabel(trackSrc_, tracks);
+
+    int N_pos = 0;
+    int N_neg = 0;
+
+    int nTracks = 0;
+
+    //define the flow vectors 
+    TComplex Q2_pos(0,0);
+    TComplex Q2_neg(0,0);
 
 
+    for( reco::TrackCollection::const_iterator cand = tracks->begin(); cand != tracks->end(); cand++){
 
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
+	double eta = cand->eta();
+	double charge = (double)cand->charge();
+	double pt = cand->pt();
+	double phi = cand->phi();
+
+	//highPurity
+	if(!cand->quality(reco::TrackBase::highPurity)) continue;
+
+	//trkNHits cut
+/*	int nhit = cand->numberOfValidHits();
+	if(nhit <= nHitCut_) continue; */
+     
+	//DCA
+	math::XYZPoint bestvtx(bestvx,bestvy,bestvz);
+	double dzbest = cand->dz(bestvtx);
+	double dxybest = cand->dxy(bestvtx);
+	double dzerror = sqrt(cand->dzError()*cand->dzError()+bestvzError*bestvzError);
+	double dxyerror = sqrt(cand->d0Error()*cand->d0Error()+bestvxError*bestvyError);
+	double dzos = dzbest/dzerror;
+	double dxyos = dxybest/dxyerror;
+	if( dzSigCut_ <= fabs(dzos) || dxySigCut_ <= fabs(dxyos) ) continue;
+
+	//ptError
+	if(fabs(cand->ptError())/cand->pt() > 0.1 ) continue;
+
+	if(fabs(eta)<2.4 && pt > 0.4){ nTracks++;}
+
+	if(2.4<=fabs(eta) || pt <= 0.3) continue;
+	
+	TComplex e(1,2*phi,1);
+	
+	if(charge>0){
+	    N_pos++;
+	    Q2_pos += e;
+	}
+	if(charge<0){
+	    N_neg++;
+	    Q2_neg += e;
+	}
+
+//	double data[7]={pt,eta,phi,charge,dzos,dxyos,(double)nhit};
+//	track_Data->Fill(pt,eta,phi,charge,dzos,dxyos);
+    }
+
+    if( nTracks < NTrkMin_ || nTracks >= NTrkMax_ ) return;
+    
+    int N_tot = N_pos + N_neg;
+    int N_diff = N_pos - N_neg;
+    double ach = (double)N_diff/N_tot;
+    asym_Dist->Fill(ach);
+    NTrkHist->Fill(nTracks);
+
+    double wt = 1.0;
+
+    double evt_avg_pos = (Q2_pos.Rho2()-nTracks)/(nTracks*(nTracks-1));
+    double evt_avg_neg = (Q2_neg.Rho2()-nTracks)/(nTracks*(nTracks-1));
+
+    double evt_wtd_pos = wt * evt_avg;
+
+    sum_wt += wt;
+    sum_wtdavg_pos += evt_wtd_pos;
+    sum_wtdavg_neg += evt_wtd_neg;
+
+    v2_pos = sum_wtdavg_pos / sum_wt;
+    v2_neg = sum_wtdavg_neg / sum_wt; 
+
 }
 
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
-V2Analyzer::beginJob()
+V2Analyzer::beginJob()    
 {
+    edm::Service<TFileService> fs;
+    TH1D::SetDefaultSumw2();
+    sum_wt = 0.0;
+    sum_wtdavg = 0.0; 
+//    track_Data = fs->make<TNtuple>("track_Data","track_Data","pt:eta:phi:charge:dzos:dxyos:nhit");
+    asym_Dist = fs->make<TH1D>("ChargeAsym","Distribution of Charge Asymmetry",21,-0.4,0.4);
+    NTrkHist = fs->make<TH1D>("NTrkHist","NTrack",5000,0,5000);
+
+    asym_Dist->SetMarkerStyle(21);
+    asym_Dist->SetMarkerSize(0.8);
+    asym_Dist->SetStats(0);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
 V2Analyzer::endJob() 
 {
+    double v2 = sum_wtdavg/sum_wt;
+    std::cout<<v2;
 }
 
 // ------------ method called when starting to processes a run  ------------
