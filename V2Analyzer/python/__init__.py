1f0bd6a0bd878e681d04a6f26776fffa56cdb6c9
Sangeon <sp35@rice.edu>
2016-06-01 16:46:43 +0200

    after crab

---
+++ V2Analyzer/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Flow/V2Analyzer/',1)[0])+'/cfipython/slc6_amd64_gcc472/Flow/V2Analyzer')
