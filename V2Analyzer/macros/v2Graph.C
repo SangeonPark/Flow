b5bf6f2f78438db790e27bb0280c14e7f8d0f997
Sangeon <sp35@rice.edu>
2016-06-01 18:04:11 +0200

    changed macros

--- V2Analyzer/macros/v2Graph.C
+++ V2Analyzer/macros/v2Graph.C
@@ -1,4 +1,31 @@
-#include "RiceStyle.h"
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+
+#include "TString.h"
+#include "TF1.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TMath.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TGraphAsymmErrors.h"
+#include "TMultiGraph.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TLine.h"
+#include "TAxis.h"
+
 
 using namespace std;
 
