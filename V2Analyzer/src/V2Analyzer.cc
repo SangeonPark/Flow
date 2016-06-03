2d0e09ab3ea966431f3cf78fd68bdf83293965cc
Sangeon <sp35@rice.edu>
2016-06-03 11:58:53 +0200

    minor change

--- V2Analyzer/src/V2Analyzer.cc
+++ V2Analyzer/src/V2Analyzer.cc
@@ -454,7 +454,7 @@ V2Analyzer::beginJob()
 
     edm::FileInPath fip1("Flow/V2Analyzer/data/TrackCorrections_HIJING_538_OFFICIAL_Mar24.root");  
     TFile f1(fip1.fullPath().c_str(),"READ");
-    effTable = (TH2D*)f1.Get("recoHist");
+    effTable = (TH2D*)f1.Get("rTotalEff3D");
 
     //list of c2 histograms
     for (Int_t i = 0; i < 5; i++){
