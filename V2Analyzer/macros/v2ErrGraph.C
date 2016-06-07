91d9f76ca51844d8f126ffbe3b665f740435c244
Sangeon <sp35@rice.edu>
2016-06-07 15:32:29 +0200

    updated macros

---
+++ V2Analyzer/macros/v2ErrGraph.C
@@ -0,0 +1,346 @@
+#include "RiceStyle.h"
+
+using namespace std;
+
+void v2ErrGraph(){
+
+	TFile* f;
+	TH1D* c2_pos_case1[5][5][2];
+	TH1D* c2_neg_case1[5][5][2];
+	TH1D* c2_tot_case1[5][5][2];
+	TH1D* c2_pos_case2[5][4][2];
+	TH1D* c2_neg_case2[5][4][2];
+	TH1D* c2_tot_case2[5][4][2];
+	TH1D* ach_hist[5];
+
+	double x[5];
+	double v2_pos_case1[5];
+	double err_pos_case1[5];
+
+	double v2_neg_case1[5];
+	double err_neg_case1[5];
+
+
+
+	double v2_pos_case2[5];
+	double err_pos_case2[5];
+
+	double v2_neg_case2[5];
+	double err_neg_case2[5];
+
+
+	double v2_diff_case1[5];
+	double err_diff_case1[5];
+
+	double v2_diff_case2[5];
+	double err_diff_case2[5];
+
+	double v2_pos_case1_sample[5][10];
+	double v2_neg_case1_sample[5][10];
+	double v2_pos_case2_sample[5][10];
+	double v2_neg_case2_sample[5][10];
+
+
+
+	double numerator;
+	double denominator;
+	double q0,q1,q2,q3;
+
+
+
+	for (int n = 0; n < 10; ++n)
+	{
+		f = new TFile(Form("../../../rootfiles/errcalcpPb/leaveout%d.root",n+1));
+
+		for (Int_t i = 0; i < 5; i++){
+			for(Int_t j = 0 ; j < 5; j++){
+
+				c2_tot_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_cos_case1",i,j));
+				c2_tot_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_sin_case1",i,j));
+				c2_pos_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_cos_case1",i,j));
+				c2_pos_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_sin_case1",i,j));
+				c2_neg_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_cos_case1",i,j));
+				c2_neg_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_sin_case1",i,j));
+
+			//c2_pos_case1[i][j][0] -> Sumw2();
+			//c2_neg_case1[i][j][0] -> Sumw2();
+			}
+			for(Int_t j=0; j<4; j++){
+				c2_tot_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_cos_case2",i,j));
+				c2_tot_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_sin_case2",i,j));
+				c2_pos_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_cos_case2",i,j));
+				c2_pos_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_sin_case2",i,j));
+				c2_neg_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_cos_case2",i,j));
+				c2_neg_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_sin_case2",i,j));
+
+			//c2_pos_case2[i][j][0] ->Sumw2();
+			//c2_neg_case2[i][j][0] ->Sumw2(); 
+
+			}
+		}
+
+		for(Int_t i=0; i<5; i++){
+
+//case1 positive
+			q0 = c2_pos_case1[i][0][0]->GetMean();
+			q1 = c2_pos_case1[i][1][0]->GetMean();
+			q2 = c2_pos_case1[i][2][0]->GetMean();
+			q3 = c2_pos_case1[i][3][0]->GetMean();
+
+	
+			numerator = q0;
+			denominator = sqrt((q1*q2)/q3);
+			v2_pos_case1_sample[i][n] = numerator/denominator;
+
+
+//case1 negative
+			q0 = c2_neg_case1[i][0][0]->GetMean();
+			q1 = c2_neg_case1[i][1][0]->GetMean();
+			q2 = c2_neg_case1[i][2][0]->GetMean();
+			q3 = c2_neg_case1[i][3][0]->GetMean();
+			numerator = q0;
+			denominator = sqrt((q1*q2)/q3);
+			v2_neg_case1_sample[i][n] = numerator/denominator;
+
+//case2 positive
+			q0 = c2_pos_case2[i][0][0]->GetMean();
+			q1 = c2_pos_case2[i][1][0]->GetMean();
+			q2 = c2_pos_case2[i][2][0]->GetMean();
+			q3 = c2_pos_case2[i][3][0]->GetMean();
+
+			numerator = q0;
+			denominator = sqrt((q1*q2)/q3);
+			v2_pos_case2_sample[i][n] = numerator/denominator;
+
+
+//case2 negative
+			q0 = c2_neg_case2[i][0][0]->GetMean();
+			q1 = c2_neg_case2[i][1][0]->GetMean();
+			q2 = c2_neg_case2[i][2][0]->GetMean();
+			q3 = c2_neg_case2[i][3][0]->GetMean();
+			numerator = q0;
+			denominator = sqrt((q1*q2)/q3);
+			v2_neg_case2_sample[i][n] = numerator/denominator;
+		}
+
+	
+
+	}
+
+
+	f = new TFile("../../../rootfiles/v2pPbcrabNo1.root");
+
+
+	for (Int_t i = 0; i < 5; i++){
+		ach_hist[i] = (TH1D*)f->Get(Form("demo/ach_%d",i+1));
+
+		for(Int_t j = 0 ; j < 5; j++){
+
+			c2_tot_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_cos_case1",i,j));
+			c2_tot_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_sin_case1",i,j));
+			c2_pos_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_cos_case1",i,j));
+			c2_pos_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_sin_case1",i,j));
+			c2_neg_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_cos_case1",i,j));
+			c2_neg_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_sin_case1",i,j));
+		}
+		for(Int_t j=0; j<4; j++){
+			c2_tot_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_cos_case2",i,j));
+			c2_tot_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_sin_case2",i,j));
+			c2_pos_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_cos_case2",i,j));
+			c2_pos_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_sin_case2",i,j));
+			c2_neg_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_cos_case2",i,j));
+			c2_neg_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_sin_case2",i,j));
+
+		}
+	}
+
+
+
+
+	for(Int_t i=0; i<5; i++){
+
+//x coordinates
+		x[i]=ach_hist[i]->GetMean();
+
+
+//case1 positive
+		q0 = c2_pos_case1[i][0][0]->GetMean();
+		q1 = c2_pos_case1[i][1][0]->GetMean();
+		q2 = c2_pos_case1[i][2][0]->GetMean();
+		q3 = c2_pos_case1[i][3][0]->GetMean();
+		
+		numerator = q0;
+		denominator = sqrt((q1*q2)/q3);
+		v2_pos_case1[i] = numerator/denominator;
+
+
+
+
+//case1 negative
+		q0 = c2_neg_case1[i][0][0]->GetMean();
+		q1 = c2_neg_case1[i][1][0]->GetMean();
+		q2 = c2_neg_case1[i][2][0]->GetMean();
+		q3 = c2_neg_case1[i][3][0]->GetMean();
+		numerator = q0;
+		denominator = sqrt((q1*q2)/q3);
+		v2_neg_case1[i] = numerator/denominator;
+
+//case2 positive
+		q0 = c2_pos_case2[i][0][0]->GetMean();
+		q1 = c2_pos_case2[i][1][0]->GetMean();
+		q2 = c2_pos_case2[i][2][0]->GetMean();
+		q3 = c2_pos_case2[i][3][0]->GetMean();
+
+		numerator = q0;
+		denominator = sqrt((q1*q2)/q3);
+		v2_pos_case2[i] = numerator/denominator;
+
+
+//case2 negative
+		q0 = c2_neg_case2[i][0][0]->GetMean();
+		q1 = c2_neg_case2[i][1][0]->GetMean();
+		q2 = c2_neg_case2[i][2][0]->GetMean();
+		q3 = c2_neg_case2[i][3][0]->GetMean();
+		numerator = q0;
+		denominator = sqrt((q1*q2)/q3);
+		v2_neg_case2[i] = numerator/denominator;
+
+
+//error bars
+		double variance_pos_case1 = 0.0;
+		double variance_pos_case2 = 0.0;		
+		double variance_neg_case1 = 0.0;
+		double variance_neg_case2 = 0.0;
+
+		for (int k = 0; k < 10; ++k)
+		{
+			variance_pos_case1 += (v2_pos_case1_sample[i][k]-v2_pos_case1[i])*(v2_pos_case1_sample[i][k]-v2_pos_case1[i]);
+			variance_pos_case2 += (v2_pos_case2_sample[i][k]-v2_pos_case2[i])*(v2_pos_case2_sample[i][k]-v2_pos_case2[i]);
+			variance_neg_case1 += (v2_neg_case1_sample[i][k]-v2_neg_case1[i])*(v2_neg_case1_sample[i][k]-v2_neg_case1[i]);
+			variance_neg_case2 += (v2_neg_case2_sample[i][k]-v2_neg_case2[i])*(v2_neg_case2_sample[i][k]-v2_neg_case2[i]);
+		}
+		variance_pos_case1 *= 0.9;
+		variance_pos_case2 *= 0.9;
+		variance_neg_case1 *= 0.9;
+		variance_neg_case2 *= 0.9;
+
+		err_pos_case1[i] = sqrt(variance_pos_case1);
+		err_pos_case2[i] = sqrt(variance_pos_case2);
+		err_neg_case1[i] = sqrt(variance_neg_case1);
+		err_neg_case2[i] = sqrt(variance_neg_case2);
+
+
+
+		v2_diff_case1[i] = v2_neg_case1[i] - v2_pos_case1[i];
+		err_diff_case1[i] = sqrt(err_neg_case1[i]*err_neg_case1[i]+err_pos_case1[i]*err_pos_case1[i]);
+
+
+		v2_diff_case2[i] = (v2_neg_case2[i] - v2_pos_case2[i]);
+		err_diff_case2[i] = sqrt(err_neg_case2[i]*err_neg_case2[i]+err_pos_case2[i]*err_pos_case2[i]);
+
+
+
+	}
+
+	TH1D* base = new TH1D("base","base",100,-0.1,0.1);
+	base->GetYaxis()->SetRangeUser(0.06,0.075);
+
+
+	TGraphErrors *gr_pos_case1 = new TGraphErrors(5,x,v2_pos_case1,NULL,err_pos_case1);
+	TGraphErrors *gr_neg_case1 = new TGraphErrors(5,x,v2_neg_case1,NULL,err_neg_case1);
+	TGraphErrors *gr_pos_case2 = new TGraphErrors(5,x,v2_pos_case2,NULL,err_pos_case2);
+	TGraphErrors *gr_neg_case2 = new TGraphErrors(5,x,v2_neg_case2,NULL,err_neg_case2);
+
+	TGraphErrors *gr_diff_case1 = new TGraphErrors(5,x,v2_diff_case1,NULL,err_diff_case1);
+	TGraphErrors *gr_diff_case2 = new TGraphErrors(5,x,v2_diff_case2,NULL,err_diff_case2);
+
+    
+
+    gr_pos_case1 -> SetName("positive tracks case1");
+    gr_neg_case1 -> SetName("negative tracks case1");
+    gr_pos_case1 -> SetTitle("positive tracks case1");
+    gr_neg_case1 -> SetTitle("negative tracks case1");
+
+    gr_diff_case1 -> SetTitle("difference case1");
+
+    gr_pos_case2 -> SetName("positive tracks case2");
+    gr_neg_case2 -> SetName("negative tracks case2");
+    gr_pos_case2 -> SetTitle("positive tracks case2");
+    gr_neg_case2 -> SetTitle("negative tracks case2");
+
+    gr_diff_case2 -> SetTitle("difference case2");
+
+
+    TCanvas* c1 = new TCanvas("c1","c1");
+    TCanvas* c2 = new TCanvas("c2","c2");
+    TCanvas* c3 = new TCanvas("c3","c3");
+    TCanvas* c4 = new TCanvas("c4","c4");
+
+    
+    c1->cd();
+    base->SetTitle("case1");
+    base->DrawClone("");
+    gr_pos_case1 -> SetMarkerStyle(20);
+    gr_pos_case1 -> SetMarkerColor(kBlue);
+    gr_neg_case1 -> SetMarkerStyle(28);
+    gr_neg_case1 -> SetMarkerColor(kRed);
+    gr_pos_case1->Draw("PSame");
+    gr_neg_case1->Draw("PSame");
+
+
+    c4->cd();
+    base->SetTitle("case2");
+    base->DrawClone("");
+    gr_pos_case2 -> SetMarkerStyle(20);
+    gr_pos_case2 -> SetMarkerColor(kBlue);
+    gr_neg_case2 -> SetMarkerStyle(28);
+    gr_neg_case2 -> SetMarkerColor(kRed);
+    gr_pos_case2->Draw("PSame");
+    gr_neg_case2->Draw("PSame");
+
+    //Define a linear function
+    TF1* fit1 = new TF1("Linear fitting case 1", "[0]+x*[1]", -0.09, 0.09);
+    fit1->SetLineColor(kRed);
+    fit1->SetLineStyle(2);
+    gr_diff_case1->Fit(fit1);
+    
+    TF1* fit2 = new TF1("Linear fitting case 2", "[0]+x*[1]", -0.09, 0.09);
+    fit2->SetLineColor(kRed);
+    fit2->SetLineStyle(2);
+    gr_diff_case2->Fit(fit2);
+
+//    c2->Divide(2,1,0,0);
+    c2->cd();
+    gr_diff_case1->GetYaxis()->SetRangeUser(-0.002,0.003);
+    gr_diff_case1->GetXaxis()->SetLimits(-0.1,0.1);
+    gr_diff_case1->GetXaxis()->SetTitle("A_{ch}");
+    gr_diff_case1->GetYaxis()->SetTitle("v_{2}(-) - v_{2}(+)");
+    gr_diff_case1->GetYaxis()->SetTitleOffset(1.1);
+    gr_diff_case1->GetXaxis()->SetTitleOffset(1.1);
+
+    TLatex* text1 = new TLatex(-0.09, 0.0025, Form("slope r = %f #pm %f",fit1->GetParameter(1),fit1->GetParError(1))) ;
+    
+    gr_diff_case1->Draw("A*");
+    text1->DrawClone("Same");
+    fit1->DrawClone("Same");
+
+    c3->cd();
+
+    TLatex* text2 = new TLatex(-0.09, 0.0025, Form("slope r = %f #pm %f",fit2->GetParameter(1),fit2->GetParError(1))) ;
+    gr_diff_case2->GetYaxis()->SetRangeUser(-0.002,0.003);
+    gr_diff_case2->GetXaxis()->SetLimits(-0.1,0.1);
+    gr_diff_case2->GetXaxis()->SetTitle("A_{ch}");
+    gr_diff_case2->GetYaxis()->SetTitle("v_{2}(-) - v_{2}(+)");
+    gr_diff_case2->GetYaxis()->SetTitleOffset(1.1);
+    gr_diff_case2->GetXaxis()->SetTitleOffset(1.1);
+
+    
+    gr_diff_case2->Draw("A*");
+    text2->DrawClone("Same");
+    fit2->DrawClone("Same");
+    
+    cout << "slope case1 : " << fit1->GetParameter(1) << endl;
+    cout << "slope case2 : " << fit2->GetParameter(1) << endl;
+
+    
+}
--- V2Analyzer/macros/v2ErrMerged.C
+++ V2Analyzer/macros/v2ErrMerged.C
@@ -28,9 +28,9 @@
 
 using namespace std;
 
-void v2Merged(){
+void v2ErrMerged(){
 
-	TFile *f = new TFile("../../../rootfiles/PbPbcrabData.root");
+	TFile* f;
 	TH1D* c2_pos_case1[5][5][2];
 	TH1D* c2_neg_case1[5][5][2];
 	TH1D* c2_tot_case1[5][5][2];
@@ -39,10 +39,127 @@ void v2Merged(){
 	TH1D* c2_neg_case2[5][4][2];
 	TH1D* c2_tot_case2[5][4][2];
 
+
 	TH1D* ach_hist[5];
 
 	TH1D* c2_pos[5][4];
 	TH1D* c2_neg[5][4];
+
+	double x[5];
+
+
+	double v2_pos[5];
+	double v2_neg[5];
+	double err_pos[5];
+	double err_neg[5];
+	double v2_diff[5];
+	double err_diff[5];
+
+
+
+	double numerator;
+	double denominator;
+	double q0,q1,q2,q3;
+
+	double esquared;
+
+	double v2_pos_sample[5][10];
+	double v2_neg_sample[5][10];
+
+
+	for (int n = 0; n < 10; ++n)
+	{
+		f = new TFile(Form("../../../rootfiles/errcalc/leaveout%d.root",n+1));
+		for (Int_t i = 0; i < 5; i++){
+			for(Int_t j = 0 ; j < 5; j++){
+
+				c2_tot_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_cos_case1",i,j));
+				c2_tot_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_sin_case1",i,j));
+				c2_pos_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_cos_case1",i,j));
+				c2_pos_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_sin_case1",i,j));
+				c2_neg_case1[i][j][0] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_cos_case1",i,j));
+				c2_neg_case1[i][j][1] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_sin_case1",i,j));
+
+			//c2_pos_case1[i][j][0] -> Sumw2();
+			//c2_neg_case1[i][j][0] -> Sumw2();
+			}
+			for(Int_t j=0; j<4; j++){
+				c2_tot_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_cos_case2",i,j));
+				c2_tot_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2tot_%d_%d_sin_case2",i,j));
+				c2_pos_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_cos_case2",i,j));
+				c2_pos_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2pos_%d_%d_sin_case2",i,j));
+				c2_neg_case2[i][j][0] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_cos_case2",i,j));
+				c2_neg_case2[i][j][1] = (TH1D*)f->Get(Form("demo/c2neg_%d_%d_sin_case2",i,j));
+
+			//c2_pos_case2[i][j][0] ->Sumw2();
+			//c2_neg_case2[i][j][0] ->Sumw2(); 
+
+			}
+		}
+
+		for (int i = 0; i < 5; ++i)
+		{
+			c2_pos[i][0] =  new TH1D(*c2_pos_case1[i][0][0]);
+			c2_pos[i][0] -> Add(c2_pos_case2[i][0][0],1.);
+
+			c2_pos[i][1] =  new TH1D(*c2_pos_case1[i][1][0]);
+			c2_pos[i][1] -> Add(c2_pos_case2[i][2][0],1.);
+
+			c2_pos[i][2] =  new TH1D(*c2_pos_case1[i][2][0]);
+			c2_pos[i][2] -> Add(c2_pos_case2[i][1][0],1.);
+
+			c2_pos[i][3] =  new TH1D(*c2_pos_case1[i][3][0]);
+			c2_pos[i][3] -> Add(c2_pos_case2[i][3][0],1.);
+
+			c2_neg[i][0] =  new TH1D(*c2_neg_case1[i][0][0]);
+			c2_neg[i][0] -> Add(c2_neg_case2[i][0][0],1.);
+
+			c2_neg[i][1] =  new TH1D(*c2_neg_case1[i][1][0]);
+			c2_neg[i][1] -> Add(c2_neg_case2[i][2][0],1.);
+
+			c2_neg[i][2] =  new TH1D(*c2_neg_case1[i][2][0]);
+			c2_neg[i][2] -> Add(c2_neg_case2[i][1][0],1.);
+
+			c2_neg[i][3] =  new TH1D(*c2_neg_case1[i][3][0]);
+			c2_neg[i][3] -> Add(c2_neg_case2[i][3][0],1.);
+
+		}
+
+		for(Int_t i=0; i<5; i++){
+
+	
+//positive
+			q0 = c2_pos[i][0]->GetMean();
+			q1 = c2_pos[i][1]->GetMean();
+			q2 = c2_pos[i][2]->GetMean();
+			q3 = c2_pos[i][3]->GetMean();
+
+
+			numerator = q0;
+			denominator = sqrt((q1*q2)/q3);
+			v2_pos_sample[i][n] = numerator/denominator;
+
+
+
+
+//negative
+			q0 = c2_neg[i][0]->GetMean();
+			q1 = c2_neg[i][1]->GetMean();
+			q2 = c2_neg[i][2]->GetMean();
+			q3 = c2_neg[i][3]->GetMean();
+			numerator = q0;
+			denominator = sqrt((q1*q2)/q3);
+			v2_neg_sample[i][n] = numerator/denominator;
+
+
+		}
+
+
+
+	}
+
+	f = new TFile("../../../rootfiles/PbPbcrabData.root");
+
 
 	for (Int_t i = 0; i < 5; i++){
 		ach_hist[i] = (TH1D*)f->Get(Form("demo/ach_%d",i+1));
@@ -99,79 +216,16 @@ void v2Merged(){
 		c2_neg[i][3] =  new TH1D(*c2_neg_case1[i][3][0]);
 		c2_neg[i][3] -> Add(c2_neg_case2[i][3][0],1.);
 
-
-
-
-		
-		
-		
 	}
-	/*
-	TCanvas* c4 = new TCanvas("c4","c4");
-	TCanvas* c5 = new TCanvas("c5","c5");
-	TCanvas* c6 = new TCanvas("c6","c6");
-
-	c4->cd();
-	c2_pos[0][0]->DrawClone();
-	c5->cd();
-	c2_pos_case1[0][0][0]->DrawClone();
-
-	c6->cd();
-	c2_pos_case2[0][0][0]->DrawClone(); */
-
-	double x[5];
-	/*
-	double v2_pos_case1[5];
-	double err_pos_case1[5];
-
-	double v2_neg_case1[5];
-	double err_neg_case1[5];
-
-	double v2_tot_case1[5];
-	double err_tot_case1[5];
-	
-	double v2_pos_case2[5];
-	double err_pos_case2[5];
-
-	double v2_neg_case2[5];
-	double err_neg_case2[5];
-
-	double v2_tot_case2[5];
-	double err_tot_case2[5];
-	
-	double v2_diff_case1[5];
-	double err_diff_case1[5];
-
-	double v2_diff_case2[5];
-	double err_diff_case2[5];
-	*/
-
-	double v2_pos[5];
-	double v2_neg[5];
-	double err_pos[5];
-	double err_neg[5];
-	double v2_diff[5];
-	double err_diff[5];
 
 
 
-	double numerator;
-	double denominator;
-	double q0,q1,q2,q3;
-	double e0,e1,e2,e3;
-	double grad0,grad1,grad2,grad3;
-	double esquared;
 	
 	
 	for(Int_t i=0; i<5; i++){
 		
-		cout << i << "th term" << endl;
-
+//x coordinates
 		x[i]=ach_hist[i]->GetMean();
-
-		cout << "x : " << x[i] <<endl;
-
-
 
 //positive
 		q0 = c2_pos[i][0]->GetMean();
@@ -179,28 +233,10 @@ void v2Merged(){
 		q2 = c2_pos[i][2]->GetMean();
 		q3 = c2_pos[i][3]->GetMean();
 
-		cout << q0 << " " << q1 << " " << q2 << " " << q3 << endl;
 
 		numerator = q0;
 		denominator = sqrt((q1*q2)/q3);
 		v2_pos[i] = numerator/denominator;
-
-		e0 = c2_pos[i][0]->GetMeanError();
-		e1 = c2_pos[i][1]->GetMeanError();
-		e2 = c2_pos[i][2]->GetMeanError();
-		e3 = c2_pos[i][3]->GetMeanError();
-
-		grad0 = 1/sqrt(q1*q2/q3);
-		grad1 = -0.5*( (q0*sqrt(q3)) / (q1*sqrt(q1*q2)) );
-		grad2 = -0.5*( (q0*sqrt(q3)) / (q2*sqrt(q1*q2)) );
-		grad3 = q0/(2*sqrt(q1*q2*q3));
-
-		esquared = grad0*grad0*e0*e0+grad1*grad1*e1*e1+grad2*grad2*e2*e2+grad3*grad3*e3*e3;
-		err_pos[i] = sqrt(esquared);
-
-		cout << "v2pos : " << v2_pos[i] << endl;
-		cout << "errpos : " << err_pos[i] << endl;
-
 
 //negative
 		q0 = c2_neg[i][0]->GetMean();
@@ -211,22 +247,25 @@ void v2Merged(){
 		denominator = sqrt((q1*q2)/q3);
 		v2_neg[i] = numerator/denominator;
 
-		e0 = c2_neg[i][0]->GetMeanError();
-		e1 = c2_neg[i][1]->GetMeanError();
-		e2 = c2_neg[i][2]->GetMeanError();
-		e3 = c2_neg[i][3]->GetMeanError();
-
-		grad0 = 1/sqrt(q1*q2/q3);
-		grad1 = -0.5*( (q0*sqrt(q3)) / (q1*sqrt(q1*q2)) );
-		grad2 = -0.5*( (q0*sqrt(q3)) / (q2*sqrt(q1*q2)) );
-		grad3 = q0/(2*sqrt(q1*q2*q3));
-
-		esquared = grad0*grad0*e0*e0+grad1*grad1*e1*e1+grad2*grad2*e2*e2+grad3*grad3*e3*e3;
-		err_neg[i] = sqrt(esquared);
 
 
+//error bars
+		double variance_pos = 0.0;
+		double variance_neg = 0.0;
+		for (int k = 0; k < 10; ++k)
+		{
+			variance_pos += (v2_pos_sample[i][k]-v2_pos[i])*(v2_pos_sample[i][k]-v2_pos[i]);
+			variance_neg += (v2_neg_sample[i][k]-v2_neg[i])*(v2_neg_sample[i][k]-v2_neg[i]);
+		}
+		variance_pos *= 0.9;
+		variance_neg *= 0.9;
+		err_pos[i] = sqrt(variance_pos);
+		err_neg[i] = sqrt(variance_neg);
+
+
+
+//difference
 		v2_diff[i] = v2_neg[i]-v2_pos[i];
-		cout << "difference : " << v2_diff[i] <<endl;
 		err_diff[i] = sqrt(err_neg[i]*err_neg[i]+err_pos[i]*err_pos[i]);
 
 		
@@ -234,54 +273,10 @@ void v2Merged(){
 
 	TH1D* base = new TH1D("base","base",100,-0.1,0.1);
 	base->GetYaxis()->SetRangeUser(0.095,0.105);
-	
-	/*
-	TGraphErrors *gr_pos_case1 = new TGraphErrors(5,x,v2_pos_case1,NULL,err_pos_case1);
-	TGraphErrors *gr_neg_case1 = new TGraphErrors(5,x,v2_neg_case1,NULL,err_neg_case1);
-	TGraphErrors *gr_tot_case1 = new TGraphErrors(5,x,v2_tot_case1,NULL,err_tot_case1);
-	TGraphErrors *gr_pos_case2 = new TGraphErrors(5,x,v2_pos_case2,NULL,err_pos_case2);
-	TGraphErrors *gr_neg_case2 = new TGraphErrors(5,x,v2_neg_case2,NULL,err_neg_case2);
-	TGraphErrors *gr_tot_case2 = new TGraphErrors(5,x,v2_tot_case2,NULL,err_tot_case2);
-	
-	TGraphErrors *gr_diff_case1 = new TGraphErrors(5,x,v2_diff_case1,NULL,err_diff_case1);
-	TGraphErrors *gr_diff_case2 = new TGraphErrors(5,x,v2_diff_case2,NULL,err_diff_case2); */
 
 	TGraphErrors *gr_pos = new TGraphErrors(5,x,v2_pos,NULL,err_pos);
 	TGraphErrors *gr_neg = new TGraphErrors(5,x,v2_neg,NULL,err_neg);
 	TGraphErrors *gr_diff = new TGraphErrors(5,x,v2_diff,NULL,err_diff);
-
-
-    /*
-    gr_pos_case1->GetYaxis()->SetRangeUser(0.095,0.115);
-    gr_pos_case1->GetXaxis()->SetLimits(-0.1,0.1);
-    gr_neg_case1->GetYaxis()->SetRangeUser(0.095,0.115);
-    gr_neg_case1->GetXaxis()->SetLimits(-0.1,0.1);
-    gr_tot_case1->GetYaxis()->SetRangeUser(0.095,0.115);
-    gr_tot_case1->GetXaxis()->SetLimits(-0.1,0.1);
-    gr_pos_case2->GetYaxis()->SetRangeUser(0.095,0.115);
-    gr_pos_case2->GetXaxis()->SetLimits(-0.1,0.1);
-    gr_neg_case2->GetYaxis()->SetRangeUser(0.095,0.115);
-    gr_neg_case2->GetXaxis()->SetLimits(-0.1,0.1);
-    gr_tot_case2->GetYaxis()->SetRangeUser(0.095,0.115);
-    gr_tot_case2->GetXaxis()->SetLimits(-0.1,0.1); */
-/*
-    gr_pos_case1 -> SetName("positive tracks case1");
-    gr_neg_case1 -> SetName("negative tracks case1");
-    gr_tot_case1 -> SetName("total tracks case1");
-    gr_pos_case1 -> SetTitle("positive tracks case1");
-    gr_neg_case1 -> SetTitle("negative tracks case1");
-    gr_tot_case1 -> SetTitle("total tracks case1");
-
-    gr_diff_case1 -> SetTitle("difference case1");
-
-    gr_pos_case2 -> SetName("positive tracks case2");
-    gr_neg_case2 -> SetName("negative tracks case2");
-    gr_tot_case2 -> SetName("total tracks case2");
-    gr_pos_case2 -> SetTitle("positive tracks case2");
-    gr_neg_case2 -> SetTitle("negative tracks case2");
-    gr_tot_case2 -> SetTitle("total tracks case2");
-
-    gr_diff_case2 -> SetTitle("difference case2"); */
 
     TCanvas* c1 = new TCanvas("c1","c1");
     TCanvas* c2 = new TCanvas("c2","c2");
@@ -303,7 +298,6 @@ void v2Merged(){
     fit1->SetLineStyle(2);
     gr_diff->Fit(fit1);
 
-//  c2->Divide(2,1,0,0);
     c2->cd();
     gr_diff->GetYaxis()->SetRangeUser(-0.002,0.003);
     gr_diff->GetXaxis()->SetLimits(-0.1,0.1);
