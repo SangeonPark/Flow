726cc805d0805a7bd714df88199150da55f57ca7
Sangeon <sp35@rice.edu>
2016-06-13 15:26:23 +0200

    configs

---
+++ V2Analyzer/python/v2analyzerCumulant_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+demo = cms.EDAnalyzer('V2AnalyzerCumulant',
+                      vertexSrc = cms.string('offlinePrimaryVertices'),
+                      trackSrc = cms.InputTag('generalTracks'),
+                      towerSrc = cms.InputTag('towerMaker'),
+                      # nHitCut = cms.int32(3),
+                      etaHFLow = cms.double(3.0),
+                      etaHFUpp = cms.double(5.0),
+                      dxySigCut = cms.double(3.0),
+                      dzSigCut = cms.double(3.0),
+                      etaCutMin = cms.double(-2.4),
+                      etaCutMax = cms.double(2.4),
+                      NTrkMin = cms.int32(185),
+                      NTrkMax = cms.int32(220),
+                      doEffCorrection = cms.bool(True),
+                      reverseBeam = cms.bool(True)
+
+                      
+)
