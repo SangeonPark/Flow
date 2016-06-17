411eb73f380bc0cfa9ade563158bc64eb8313e17
Sangeon <sp35@rice.edu>
2016-06-17 12:16:59 +0200

    changed folders

--- V2Analyzer/macros/SPGraph.C
+++ V2Analyzer/macros/SPGraph.C
@@ -4,7 +4,7 @@ using namespace std;
 
 void SPGraph(){
 
-	TFile *f = new TFile("../../../rootfiles/SPpPbMerged.root");
+	TFile *f = new TFile("../../../rootfiles/V3SPmethod.root");
 	TH1D* c2_pos_case1[5][4][2];
 	TH1D* c2_neg_case1[5][4][2];
 	TH1D* c2_tot_case1[5][4][2];
@@ -101,7 +101,7 @@ void SPGraph(){
 	TGraph *gr_diff = new TGraph(5,x,v2_diff);
 
 	TH1D* base = new TH1D("base","base",100,-0.1,0.1);
-	base->GetYaxis()->SetRangeUser(0.06,0.08);
+	base->GetYaxis()->SetRangeUser(-0.08,0.08);
 	base->GetXaxis()->SetTitle("Observed A_{ch}");
 	base->GetYaxis()->SetTitle("v_{2}");
 	base->SetStats(0);
