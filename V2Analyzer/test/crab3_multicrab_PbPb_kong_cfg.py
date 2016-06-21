ae8cd44ccc2c7ba7d503adcbda147c082040c6b5
Sangeon <sp35@rice.edu>
2016-06-21 18:42:13 +0200

    5.02 TeV PbPb Data

--- V2Analyzer/test/crab3_multicrab_PbPb_kong_cfg.py
+++ V2Analyzer/test/crab3_multicrab_PbPb_kong_cfg.py
@@ -4,18 +4,18 @@ config = config()
 import FWCore.ParameterSet.Config as cms
 #load the cfi file and rewrite cross section parameter each time:
 process = cms.Process('Demo')
-process.load("CMEandCorrelation.ThreePointCorrelator.threepointcorrelatoretagap_cfi")
+process.load("Flow.V2Analyzer.PbPbCumulant_cfi")
 
-ntrkRange = [0,35,60,90,120,150,185,220,260]
+ntrkRange = [60,80,100,120,140]
 
-outputName = "multicrab_CME_QvsdEta_PbPb_50_100_v1"
+outputName = "multicrab_v2vsAch_PbPb_Cumulant_502TeV"
 
 config.General.transferOutputs = True
 config.General.transferLogs = True
 config.JobType.allowUndistributedCMSSW = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'threepointcorrelatoretagap_cfg.py'
+config.JobType.psetName = 'PbPb_502TeV_cfg.py'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 15
@@ -23,7 +23,7 @@ config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
 config.Data.outputDatasetTag = outputName
 
-config.Site.storageSite = 'T2_US_MIT'
+config.Site.storageSite = 'T3_US_Rice'
 
 if __name__ == '__main__':
    from CRABAPI.RawCommand import crabCommand
@@ -40,14 +40,14 @@ if __name__ == '__main__':
       except ClientException as cle:
           print "Failed submitting task: %s" % (cle)
    
-   for num in range(0,8):
+   for num in range(0,5):
 		
-	print 'double check that multiplicity range is fram %r to %r' % (ntrkRange[num],ntrkRange[num+1])
+	print 'double check that centrality range is fram %r to %r' % (ntrkRange[num],ntrkRange[num+1])
       		
 	process.ana.Nmin = ntrkRange[num]
 	process.ana.Nmax = ntrkRange[num+1]
        	RequestName = outputName + "_" + str(num)
-       	DataSetName = '/HIMinBiasUPC/davidlw-PR2011_MBPPRereco_TRKANASKIM_v6-ccf03100d177f42de0f9cdc7627799d3/USER'
+       	DataSetName = '/HIMinimumBias5/HIRun2015-02May2016-v1/AOD'
        	config.General.requestName = RequestName
        	config.Data.inputDataset = DataSetName
 submit(config)
