ea91a593136cd59c062fb02e350b3aab6e5c967e
Sangeon <sp35@rice.edu>
2016-07-12 19:48:06 +0200

    error fix

--- V2Analyzer/test/crab_PbPbCumulant.py
+++ V2Analyzer/test/crab_PbPbCumulant.py
@@ -3,7 +3,7 @@
 
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
-outputName = 'PbPb_Cumulant_v3_40_50_cent_0712'
+outputName = 'PbPb_Cumulant_v3_50_60_cent_0712'
 config.General.requestName = outputName
 config.General.workArea = outputName
 config.General.transferOutputs = True
