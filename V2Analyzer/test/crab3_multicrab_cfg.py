9d4920633d8288860ba72c5e300115241385f460
Sangeon <sp35@rice.edu>
2016-07-24 21:39:47 +0200

    No eff. corr. on Ach

--- V2Analyzer/test/crab3_multicrab_cfg.py
+++ V2Analyzer/test/crab3_multicrab_cfg.py
@@ -4,16 +4,16 @@ config = config()
 import FWCore.ParameterSet.Config as cms
 #load the cfi file and rewrite cross section parameter each time:
 process = cms.Process('Demo')
-process.load("Flow.V2Analyzer.v2analyzerCumulant_cfi")
+process.load("Flow.V2Analyzer.v3analyzerCumulant_cfi")
 
 config.General.transferOutputs = True
 config.General.transferLogs = True
 config.JobType.allowUndistributedCMSSW = True
 
-outputName = 'multicrab_CMW_pPb_v2_no_eff_corr_on_Ach_185_260'
+outputName = 'multicrab_CMW_pPb_v3_no_eff_corr_on_Ach_185_260'
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'v2analyzerCumulant_cfg.py'
+config.JobType.psetName = 'v3analyzerCumulant_cfg.py'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
