826a53e4cfa9bc40bc5179782f1d8f2f0400c6a0
Sangeon <sp35@rice.edu>
2016-08-03 11:42:10 +0900

    ntrk weighting

--- V2Analyzer/test/crab3_multicrab_MC_PbPb_cfg.py
+++ V2Analyzer/test/crab3_multicrab_MC_PbPb_cfg.py
@@ -6,10 +6,10 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process('Demo')
 process.load("Flow.V2Analyzer.asymmetryscatterplot_cfi")
 
-ntrkRange = [400,410]
+ntrkRange = [800,810]
 reweightTable = ["table_0","table_1","table_2"]
 
-outputName = "multicrab_CMW_MC_smallntrackrange_400_410"
+outputName = "multicrab_CMW_MC_smallntrackrange_800_810"
 
 config.General.transferOutputs = True
 config.General.transferLogs = True
