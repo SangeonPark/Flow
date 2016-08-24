b0b708a11f7880827ee624b92df1bc4caebd999c
Sangeon <sp35@rice.edu>
2016-08-23 22:24:27 -0500

    closure

--- V2Analyzer/test/v2analyzerCumulant_cfg.py
+++ V2Analyzer/test/v2analyzerCumulant_cfg.py
@@ -26,8 +26,8 @@ process.source = cms.Source("PoolSource",
 )
 
 #loads analyzer
-process.load("Flow.V2Analyzer.v2analyzerCumulant_cfi")
-process.TFileService = cms.Service("TFileService",fileName = cms.string("v2_systematics_loose_0823.root"))
+process.load("Flow.V2Analyzer.closure_cfi")
+process.TFileService = cms.Service("TFileService",fileName = cms.string("v2_pPbclosure_0823.root"))
 
 
 process.p = cms.Path(process.hltHM*process.demo)
