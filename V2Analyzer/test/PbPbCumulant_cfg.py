bc21df6726cdf9805ee599a4f4648bce0c5b236d
Sangeon <sp35@rice.edu>
2016-08-25 00:08:16 -0500

    systematics

--- V2Analyzer/test/PbPbCumulant_cfg.py
+++ V2Analyzer/test/PbPbCumulant_cfg.py
@@ -32,7 +32,7 @@ process.source = cms.Source("PoolSource",
 
 #loads analyzer
 process.load("Flow.V2Analyzer.v2analyzerCumulant_cfi")
-process.TFileService = cms.Service("TFileService",fileName = cms.string("systematics_trkselection_tight.root"))
+process.TFileService = cms.Service("TFileService",fileName = cms.string("systematics_trkselection_loose.root"))
 
 
 process.p = cms.Path(process.demo)
