40b1fb79798332040d26bad1510be2b27e8e904a
Sangeon <sp35@rice.edu>
2016-10-17 23:38:07 -0500

    smearing width test

--- V2Analyzer/test/crab3_multicrab_PbPb_goldenjson_and_trackeronly.py
+++ V2Analyzer/test/crab3_multicrab_PbPb_goldenjson_and_trackeronly.py
@@ -31,7 +31,7 @@ efftableNameList = ["eff_5","eff_5","eff_5","eff_5"]
 efftablePathList = ["Flow/V2Analyzer/data/Hydjet_PbPb_eff_v1_loose.root","Flow/V2Analyzer/data/Hydjet_PbPb_eff_v1_loose.root"]
 
 
-outputName = "multicrab_CMW_closuretest_smearing_1003"
+outputName = "multicrab_CMW_closuretest_smearing_uniformtest"
 
 config.General.transferOutputs = True
 config.General.transferLogs = True
