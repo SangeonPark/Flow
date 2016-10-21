d710f12c6e95d29f0805cf70c4abb1212b145c8f
Sangeon <sp35@rice.edu>
2016-10-21 00:47:52 -0500

    ntrk

--- V2Analyzer/test/crab3_multicrab_PbPb_goldenjson_and_trackeronly.py
+++ V2Analyzer/test/crab3_multicrab_PbPb_goldenjson_and_trackeronly.py
@@ -9,8 +9,8 @@ process.load("Flow.V2Analyzer.closure_cfi")
 #ntrkRange = [200,400,800,1500]
 #ntrkLow = [60,70,80,90,100,120,140,160]
 #ntrkUpp = [70,80,90,100,120,140,160,180]
-#ntrkLow = [90,120,150,185,220,260,300,400]
-#ntrkUpp = [120,150,185,220,260,300,400,500]
+ntrkLow = [90,120,150,185,220,260,300,400]
+ntrkUpp = [120,150,185,220,260,300,400,500]
 
 #ntrkLow = [60,80,100,120,140,160]
 #ntrkUpp = [80,100,120,140,160,180]
@@ -21,20 +21,20 @@ process.load("Flow.V2Analyzer.closure_cfi")
 #ntrkLow = [975,950,925,900]
 #ntrkUpp = [1025,1050,1075,1100]
 
-ntrkLow = [60,90,120,155,190,230,270,360,450,650,950,1050]
-ntrkUpp = [120,150,180,215,250,290,330,440,550,750,1050,1150]
+#ntrkLow = [60,90,120,155,190,230,270,360,450,650,950,1050]
+#ntrkUpp = [120,150,180,215,250,290,330,440,550,750,1050,1150]
 
 
-#efftableNameList = ["eff_1","eff_2","eff_2","eff_2","eff_3","eff_3","eff_3","eff_4"]
+efftableNameList = ["eff_1","eff_2","eff_2","eff_2","eff_3","eff_3","eff_3","eff_4"]
 #efftableNameList = ["eff_5","eff_4","eff_4","eff_3","eff_1","eff_1"]
 #efftableNameList = ["eff_3","eff_4","eff_5"]
 #efftableNameList = ["eff_5","eff_5","eff_5","eff_5"]
-efftableNameList = ["eff_1","eff_1","eff_2","eff_2","eff_3","eff_3","eff_3","eff_3","eff_4","eff_4","eff_5","eff_5"]
+#efftableNameList = ["eff_1","eff_1","eff_2","eff_2","eff_3","eff_3","eff_3","eff_3","eff_4","eff_4","eff_5","eff_5"]
 
 efftablePathList = ["Flow/V2Analyzer/data/Hydjet_PbPb_eff_v1_loose.root","Flow/V2Analyzer/data/Hydjet_PbPb_eff_v1_loose.root"]
 
 
-outputName = "multicrab_CMW_closuretest_smearing_functionderivation_ver2"
+outputName = "multicrab_CMW_closuretest_smearing_functionderivation_ntrk"
 
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -70,7 +70,7 @@ if __name__ == '__main__':
                   "/HIMinimumBias5/davidlw-RecoSkim2015_pprereco_TrackerOnly_v5-70836070e3530d592901940b96c951fe/USER"]       
    
    for num in range(0,2):
-      for paths in range(4,12):
+      for paths in range(0,8):
          print 'double check that we are using sample %r ' % (sampleName[num])		
          print 'double check that ntrk range is fram %r to %r' % (ntrkLow[paths],ntrkUpp[paths])
          print 'double check that we are using %r' % (efftableNameList[paths])
