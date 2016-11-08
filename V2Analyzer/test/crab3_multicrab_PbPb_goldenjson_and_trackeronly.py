8de7168095115b2974b3bf818bd681dab2edab04
Sangeon <sp35@rice.edu>
2016-11-07 22:53:02 -0600

    apple to apple

--- V2Analyzer/test/crab3_multicrab_PbPb_goldenjson_and_trackeronly.py
+++ V2Analyzer/test/crab3_multicrab_PbPb_goldenjson_and_trackeronly.py
@@ -43,7 +43,7 @@ efftableNameList = ["eff_5"]
 efftablePathList = ["Flow/V2Analyzer/data/Hydjet_PbPb_eff_v1_loose.root","Flow/V2Analyzer/data/Hydjet_PbPb_eff_v1_loose.root"]
 
 
-outputName = "multicrab_CMW_selfcorrelation_ScalarProduct_v2"
+outputName = "multicrab_CMW_appletoapple_ScalarProduct_ALICE"
 
 config.General.transferOutputs = True
 config.General.transferLogs = True
