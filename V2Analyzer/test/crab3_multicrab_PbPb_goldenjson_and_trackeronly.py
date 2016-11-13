77b4ec4811dd94e4121f452bd8eee0995b63bd9d
Sangeon <sp35@rice.edu>
2016-11-13 17:09:57 -0600

    systematics

--- V2Analyzer/test/crab3_multicrab_PbPb_goldenjson_and_trackeronly.py
+++ V2Analyzer/test/crab3_multicrab_PbPb_goldenjson_and_trackeronly.py
@@ -15,8 +15,8 @@ process.load("Flow.V2Analyzer.v2analyzerCumulant_cfi")
 #ntrkLow = [60,80,100,120,140,160]
 #ntrkUpp = [80,100,120,140,160,180]
 
-ntrkLow = [60,80]
-ntrkUpp = [80,100]
+ntrkLow = [60]
+ntrkUpp = [80]
 
 
 #ntrkLow = [80,140,160]
@@ -34,7 +34,7 @@ ntrkUpp = [80,100]
 
 #efftableNameList = ["eff_1","eff_2","eff_2","eff_2","eff_3","eff_3","eff_3","eff_4"]
 #efftableNameList = ["eff_5","eff_4","eff_3","eff_2","eff_1","eff_1"]
-efftableNameList = ["eff_5","eff_4"]
+efftableNameList = ["eff_5"]
 
 #efftableNameList = ["eff_3","eff_4","eff_5"]
 #efftableNameList = ["eff_5","eff_5","eff_5","eff_5"]
@@ -43,7 +43,7 @@ efftableNameList = ["eff_5","eff_4"]
 efftablePathList = ["Flow/V2Analyzer/data/Hydjet_PbPb_eff_v1_loose.root","Flow/V2Analyzer/data/Hydjet_PbPb_eff_v1_loose.root"]
 
 
-outputName = "multicrab_CMW_v2_2ParticleCumulant"
+outputName = "multicrab_CMW_v2_Cumulant_Systematics_VaryAchCut_TrkSelection_Tight"
 
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -79,7 +79,7 @@ if __name__ == '__main__':
                   "/HIMinimumBias5/davidlw-RecoSkim2015_pprereco_TrackerOnly_v5-70836070e3530d592901940b96c951fe/USER"]       
    
    for num in range(0,2):
-      for paths in range(0,2):
+      for paths in range(0,1):
          print 'double check that we are using sample %r ' % (sampleName[num])		
          print 'double check that ntrk range is fram %r to %r' % (ntrkLow[paths],ntrkUpp[paths])
          print 'double check that we are using %r' % (efftableNameList[paths])
