7d742e8b2a389278b51696ed25bc31a899f65b5d
Sangeon <sp35@rice.edu>
2016-11-13 20:24:21 -0600

    systematics

--- V2Analyzer/python/v2analyzerCumulant_cfi.py
+++ V2Analyzer/python/v2analyzerCumulant_cfi.py
@@ -20,11 +20,11 @@ demo = cms.EDAnalyzer('V2AnalyzerCumulant',
                       #don't change this part
                       etaHFLow = cms.double(3.0),
                       etaHFUpp = cms.double(5.0),
-                      offlineDCA = cms.untracked.double(5.0),
+                      offlineDCA = cms.untracked.double(3.0),
                       offlineptErr = cms.untracked.double(0.1),
 
                       vzLow = cms.untracked.double(0.0),
-                      vzHigh = cms.untracked.double(15.0),                      
+                      vzHigh = cms.untracked.double(3.0),                      
                       #Check whether to apply eff. correction 
                       doEffCorrection = cms.bool(True),
                       doAchEffCorrection = cms.bool(True),
